316e3fcb16b4f4247a31ee87ddd6977b2c74d004
Tera <24725862+teraa@users.noreply.github.com>
2020-11-22 16:47:48 +0100

    refactor

--- src/Common/AsyncEventExtensions.cs
+++ src/Common/AsyncEventExtensions.cs
@@ -2,38 +2,26 @@ using System;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
+
 namespace Twitch
 {
     public static class AsyncEventExtensions
     {
-        public static async Task InvokeAsync(this Func<Task> eventHandler)
-        {
-            var list = eventHandler.GetInvocationList();
-            for (int i = 0; i < list.Length; i++)
-            {
-                var sub = list[i];
-                try
-                {
-                    var task = (Task)sub.Method.Invoke(sub.Target, null)!;
-                    await task.ConfigureAwait(false);
-                }
-                catch (TargetInvocationException ex)
-                {
-                    ExceptionDispatchInfo.Capture(ex.InnerException!).Throw();
-                }
-            }
-        }
+        public static Task InvokeAsync(this Func<Task> eventHandler)
+            => InvokePrivateAsync(eventHandler, null);
 
-        public static async Task InvokeAsync<T>(this Func<T, Task> eventHandler, T arg)
-            where T : class
+        public static Task InvokeAsync<T>(this Func<T, Task> eventHandler, T arg) where T : class
+            => InvokePrivateAsync(eventHandler, new[] { arg });
+
+        private static async Task InvokePrivateAsync(MulticastDelegate d, object?[]? parameters)
         {
-            var list = eventHandler.GetInvocationList();
+            var list = d.GetInvocationList();
             for (int i = 0; i < list.Length; i++)
             {
                 var sub = list[i];
                 try
                 {
-                    var task = (Task)sub.Method.Invoke(sub.Target, new[] { arg })!;
+                    var task = (Task)sub.Method.Invoke(sub.Target, parameters)!;
                     await task.ConfigureAwait(false);
                 }
                 catch (TargetInvocationException ex)
