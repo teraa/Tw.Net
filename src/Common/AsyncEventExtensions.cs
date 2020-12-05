85397ce71847f74691f87102440d772a93c44399
Tera <24725862+teraa@users.noreply.github.com>
2020-12-06 00:16:45 +0100

    update AsyncEventExtensions

--- src/Common/AsyncEventExtensions.cs
+++ src/Common/AsyncEventExtensions.cs
@@ -7,31 +7,18 @@ namespace Twitch
 {
     internal static class AsyncEventExtensions
     {
-        public static Task InvokeAsync(this Func<Task> eventHandler)
-            => InvokePrivateAsync(eventHandler, null);
-
-        public static Task InvokeAsync<T>(this Func<T, Task> eventHandler, T? arg)
-            where T : class
-            => InvokePrivateAsync(eventHandler, new[] { arg });
-
-        public static Task InvokeAsync<T1, T2>(this Func<T1, T2, Task> eventHandler, T1? arg1, T2? arg2)
-            where T1 : class
-            where T2 : class
-            => InvokePrivateAsync(eventHandler, new object?[] { arg1, arg2 });
-
-        private static async Task InvokePrivateAsync(MulticastDelegate d, object?[]? parameters)
+        public static async Task InvokeAsync(this MulticastDelegate eventDelegate, object?[]? parameters)
         {
-            if (d is null)
-                throw new ArgumentNullException(nameof(d));
+            if (eventDelegate is null)
+                throw new ArgumentNullException(nameof(eventDelegate));
 
-            var list = d.GetInvocationList();
-            for (int i = 0; i < list.Length; i++)
+            var invocationList = eventDelegate.GetInvocationList();
+            for (int i = 0; i < invocationList.Length; i++)
             {
-                var sub = list[i];
+                var sub = invocationList[i];
                 try
                 {
-                    var task = (Task)sub.Method.Invoke(sub.Target, parameters)!;
-                    await task.ConfigureAwait(false);
+                    await ((Task)sub.Method.Invoke(sub.Target, parameters)!).ConfigureAwait(false);
                 }
                 catch (TargetInvocationException ex)
                 {
