9de500c40e7f8cfd2a0bed4f654cfd159d6f16e5
Tera <24725862+teraa@users.noreply.github.com>
2020-12-06 21:28:05 +0100

    update (dis)connect logic

--- src/Irc/TwitchIrcClient.cs
+++ src/Irc/TwitchIrcClient.cs
@@ -18,17 +18,21 @@ namespace Twitch.Irc
         private readonly TimeSpan _eventWarningTimeout = TimeSpan.FromSeconds(2);
         private readonly ISocketClient _client;
         private readonly ILogger<TwitchIrcClient>? _logger;
+        private readonly SemaphoreSlim _connectSem;
         private readonly AsyncEventInvoker _eventInvoker;
         private readonly Timer _pingTimer;
         private string? _login;
         private string? _token;
         private Task? _listenerTask;
+        private CancellationTokenSource? _stoppingTokenSource;
+        private CancellationTokenSource? _disconnectTokenSource;
 
         public TwitchIrcClient(ISocketClient client, ILogger<TwitchIrcClient>? logger = null)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
             _logger = logger;
 
+            _connectSem = new SemaphoreSlim(1, 1);
             _eventInvoker = new AsyncEventInvoker(_eventWarningTimeout, _logger);
 
             _pingTimer = new Timer();
@@ -77,31 +81,43 @@ namespace Twitch.Irc
 
         public async Task ConnectAsync(string login, string token, CancellationToken cancellationToken = default)
         {
-            _login = login ?? throw new ArgumentNullException(nameof(login));
-            _token = token ?? throw new ArgumentNullException(nameof(token));
-
-            await _client.ConnectAsync(cancellationToken).ConfigureAwait(false);
-            _listenerTask = ListenAsync(cancellationToken);
-
-            var caps = "twitch.tv/tags twitch.tv/commands";
-            if (RequestMembershipCapability)
-                caps += " twitch.tv/membership";
-
-            var capReq = new IrcMessage
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
             {
-                Command = IrcCommand.CAP,
-                Arg = "REQ",
-                Content = (caps, null)
-            };
+                _login = login ?? throw new ArgumentNullException(nameof(login));
+                _token = token ?? throw new ArgumentNullException(nameof(token));
 
-            await SendAsync(capReq).ConfigureAwait(false);
+                _stoppingTokenSource = new CancellationTokenSource();
 
-            await LoginAsync(cancellationToken).ConfigureAwait(false);
+                _disconnectTokenSource = CancellationTokenSource.CreateLinkedTokenSource(_stoppingTokenSource.Token);
 
-            _pingTimer.Interval = _pingInterval.TotalMilliseconds;
-            _pingTimer.Enabled = true;
+                await _client.ConnectAsync(cancellationToken).ConfigureAwait(false);
+                _listenerTask = ListenAsync();
 
-            await _eventInvoker.InvokeAsync(Connected, nameof(Connected)).ConfigureAwait(false);
+                var caps = "twitch.tv/tags twitch.tv/commands";
+                if (RequestMembershipCapability)
+                    caps += " twitch.tv/membership";
+
+                var capReq = new IrcMessage
+                {
+                    Command = IrcCommand.CAP,
+                    Arg = "REQ",
+                    Content = (Text: caps, Ctcp: null)
+                };
+
+                await SendAsync(capReq).ConfigureAwait(false);
+
+                await LoginAsync(cancellationToken).ConfigureAwait(false);
+
+                _pingTimer.Interval = _pingInterval.TotalMilliseconds;
+                _pingTimer.Enabled = true;
+
+                await _eventInvoker.InvokeAsync(Connected, nameof(Connected)).ConfigureAwait(false);
+            }
+            finally
+            {
+                _connectSem.Release();
+            }
         }
         public Task ConnectAsync(CancellationToken cancellationToken = default)
         {
@@ -110,22 +126,47 @@ namespace Twitch.Irc
 
         public async Task DisconnectAsync()
         {
-            _pingTimer.Enabled = false;
+            await _connectSem.WaitAsync().ConfigureAwait(false);
+            try
+            {
+                _stoppingTokenSource?.Cancel();
 
-            // TODO: cancel the token
+                if (_listenerTask is Task task)
+                {
+                    try
+                    {
+                        await task.ConfigureAwait(false);
+                    }
+                    catch (OperationCanceledException) { }
+                }
+
+                _listenerTask = null;
+            }
+            finally
+            {
+                _connectSem.Release();
+            }
+        }
+
+        private async Task DisconnectInternalAsync()
+        {
+            _pingTimer.Enabled = false;
             _client.Disconnect();
 
-            if (_listenerTask is Task task)
+            await _eventInvoker.InvokeAsync(Disconnected, nameof(Disconnected)).ConfigureAwait(false);
+
+            if (_stoppingTokenSource?.IsCancellationRequested == false)
             {
                 try
                 {
-                    await task.ConfigureAwait(false);
+                    // TODO: Delay
+                    await ConnectAsync(_login!, _token!, default).ConfigureAwait(false);
                 }
-                catch (OperationCanceledException) { }
+                catch (Exception ex)
+                {
+                    _logger?.LogError(ex, "Exception thrown while trying to reconnect");
+                }
             }
-
-            _listenerTask = null;
-
         }
 
         public async Task SendRawAsync(string message)
@@ -141,13 +182,13 @@ namespace Twitch.Irc
             await _eventInvoker.InvokeAsync(IrcMessageSent, nameof(IrcMessageSent), message).ConfigureAwait(false);
         }
 
-        private async Task ListenAsync(CancellationToken cancellationToken)
+        private async Task ListenAsync()
         {
             try
             {
-                while (!cancellationToken.IsCancellationRequested)
+                while (_disconnectTokenSource?.IsCancellationRequested == false)
                 {
-                    var message = await _client.ReadAsync(cancellationToken).ConfigureAwait(false);
+                    var message = await _client.ReadAsync(_disconnectTokenSource.Token).ConfigureAwait(false);
                     if (message is not null)
                     {
                         await HandleRawMessageAsync(message).ConfigureAwait(false);
@@ -159,8 +200,11 @@ namespace Twitch.Irc
             catch (Exception ex)
             {
                 _logger?.LogError(ex, "Exception in listener task");
-                // TODO: restart
             }
+
+            _disconnectTokenSource?.Cancel();
+
+            await DisconnectInternalAsync().ConfigureAwait(false);
         }
 
         private async Task<IrcMessage?> GetNextMessageAsync(Func<IrcMessage, bool> predicate, TimeSpan timeout, CancellationToken cancellationToken)
@@ -209,12 +253,12 @@ namespace Twitch.Irc
                 if (response is null)
                 {
                     _logger?.LogWarning("Login timed out after " + _loginTimeout.ToString());
-                    // TODO: login timed out, reconnect
+                    _disconnectTokenSource?.Cancel();
                 }
                 else if (response.Command == IrcCommand.NOTICE)
                 {
                     _logger?.LogError(response.Content?.Text ?? "Login failed");
-                    // TODO: login failed, disconnect
+                    _stoppingTokenSource?.Cancel();
                 }
                 else
                 {
@@ -284,14 +328,16 @@ namespace Twitch.Irc
 
                 await SendAsync(request).ConfigureAwait(false);
 
-                // TODO: token
+                if (_disconnectTokenSource?.Token is not CancellationToken cancellationToken)
+                    return;
+
                 var response = await GetNextMessageAsync(x => x.Command == IrcCommand.PONG && x.Content?.Text == text,
-                    _loginTimeout, default).ConfigureAwait(false);
+                    _loginTimeout, cancellationToken).ConfigureAwait(false);
 
                 if (response is null)
                 {
                     _logger?.LogWarning($"No PONG received within {_pongTimeout}");
-                    // TODO: reconnect
+                    _disconnectTokenSource?.Cancel();
                 }
             }
             catch (Exception ex)
