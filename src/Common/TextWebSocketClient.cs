bb1e3ea11b4a535484c189a1b1edbff646fd7cca
Tera <24725862+teraa@users.noreply.github.com>
2020-12-12 19:34:49 +0100

    add missing license

--- src/Common/TextWebSocketClient.cs
+++ src/Common/TextWebSocketClient.cs
@@ -50,6 +50,39 @@ namespace Twitch
             catch { }
         }
 
+        #region license
+        /*
+        ReadAsync and ReadBufferAsync methods use modified implementation of
+        System.IO.StreamReader's ReadLineAsyncInternal and ReadBufferAsync methods.
+
+        The MIT License (MIT)
+
+        Copyright (c) .NET Foundation and Contributors
+
+        All rights reserved.
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+        */
+        #endregion
+
+        // https://source.dot.net/#System.Private.CoreLib/StreamReader.cs,837
+        // https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/IO/StreamReader.cs#L837
         public async Task<string?> ReadAsync(CancellationToken cancellationToken = default)
         {
             int read;
@@ -119,6 +152,8 @@ namespace Twitch
             return sb.ToString();
         }
 
+        // https://source.dot.net/#System.Private.CoreLib/StreamReader.cs,1217
+        // https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/IO/StreamReader.cs#L1217
         private async Task<int> ReadBufferAsync(CancellationToken cancellationToken)
         {
             if (_client is null)
