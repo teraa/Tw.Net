a85b307592b1e79fc6a75c7c3dbcccc0b826476e
Tera <24725862+teraa@users.noreply.github.com>
2021-01-01 04:12:26 +0100

    internal init

--- src/PubSub/Models/User.cs
+++ src/PubSub/Models/User.cs
@@ -2,7 +2,7 @@ namespace Twitch.PubSub
 {
     public class User
     {
-        public string Id { get; init; } = null!;
-        public string Login { get; init; } = null!;
+        public string Id { get; internal init; } = null!;
+        public string Login { get; internal init; } = null!;
     }
 }
