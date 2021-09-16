f4e7f90ce05ce7ac432cad5501dfde7fac5d7d93
Tera <24725862+teraa@users.noreply.github.com>
2021-09-17 01:04:35 +0200

    add ServiceCollectionExtensions

---
+++ src/Irc/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Twitch.Clients;
+
+namespace Twitch.Irc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTwitchIrcClient(this IServiceCollection serviceCollection, Action<TwitchIrcClient>? optionsAction = null)
+        {
+            return serviceCollection.AddSingleton<TwitchIrcClient>(serviceProvider => CreateTwitchIrcClient(serviceProvider, optionsAction));
+        }
+
+        public static TwitchIrcClient CreateTwitchIrcClient(IServiceProvider serviceProvider, Action<TwitchIrcClient>? optionsAction = null)
+        {
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            var uri = new Uri("wss://irc-ws.chat.twitch.tv:443");
+            var socket = new WebSocketClient(uri, loggerFactory.CreateLogger<WebSocketClient>());
+            var client = new TwitchIrcClient(socket, loggerFactory.CreateLogger<TwitchIrcClient>());
+
+            optionsAction?.Invoke(client);
+
+            return client;
+        }
+    }
+}
---
+++ src/PubSub/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Twitch.Clients;
+
+namespace Twitch.PubSub
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTwitchPubSubClient(this IServiceCollection serviceCollection, Action<TwitchPubSubClient>? optionsAction = null)
+        {
+            return serviceCollection.AddSingleton<TwitchPubSubClient>(serviceProvider => CreateTwitchPubSubClient(serviceProvider, optionsAction));
+        }
+
+        public static TwitchPubSubClient CreateTwitchPubSubClient(IServiceProvider serviceProvider, Action<TwitchPubSubClient>? optionsAction = null)
+        {
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            var uri = new Uri("wss://pubsub-edge.twitch.tv");
+            var socket = new WebSocketClient(uri, loggerFactory.CreateLogger<WebSocketClient>());
+            var client = new TwitchPubSubClient(socket, loggerFactory.CreateLogger<TwitchPubSubClient>());
+
+            optionsAction?.Invoke(client);
+
+            return client;
+        }
+    }
+}
