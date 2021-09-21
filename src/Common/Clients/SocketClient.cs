8249742b313a135d3046d2db6a5cdb713a9e59c7
Tera <24725862+teraa@users.noreply.github.com>
2021-09-21 23:18:48 +0200

    update socket clients

--- src/Common/Clients/SocketClient.cs
+++ src/Common/Clients/SocketClient.cs
@@ -11,94 +11,159 @@ using Microsoft.Extensions.Logging;
 
 namespace Twitch.Clients
 {
-    // TODO: finish, public
-    internal class SocketClient : ISocketClient
+    public class SocketClient : ISocketClient, IDisposable
     {
         private readonly ILogger<SocketClient> _logger;
         private readonly Socket _socket;
+        private readonly SemaphoreSlim _connectSem;
         private Stream? _stream;
         private PipeReader? _pipeReader;
         private Task? _readTask;
+        private State _state;
+        private EndPoint _endpoint;
 
         public SocketClient(EndPoint endpoint, ProtocolType protocol, bool ssl, ILogger<SocketClient> logger)
         {
-            Endpoint = endpoint;
+            _endpoint = endpoint ?? throw new ArgumentNullException(nameof(endpoint));
             Protocol = protocol;
             UseSsl = ssl;
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _socket = new Socket(SocketType.Stream, protocol);
+            _connectSem = new SemaphoreSlim(1, 1);
+            _state = State.None;
         }
 
         public event Func<ReadOnlySequence<byte>, ValueTask>? Received;
-
-#pragma warning disable CS0067 // The event is never used
         public event Func<Exception?, ValueTask>? ClosedUnexpectedly;
-#pragma warning restore CS0067
 
-        public EndPoint Endpoint { get; set; }
+        private enum State
+        {
+            None,
+            Open,
+            CloseRequested,
+            Closed,
+        }
+
+        public EndPoint Endpoint
+        {
+            get => _endpoint;
+            set => _endpoint = value ?? throw new ArgumentNullException(nameof(Endpoint));
+        }
+
         public ProtocolType Protocol { get; set; }
         public bool UseSsl { get; set; }
+        public byte[] MessageDelimiter { get; set; } = new byte[] { (byte)'\r', (byte)'\n' };
 
         public async Task ConnectAsync(CancellationToken cancellationToken = default)
         {
-            await _socket.ConnectAsync(Endpoint, cancellationToken).ConfigureAwait(false);
-
-            _stream = new NetworkStream(_socket);
-
-            if (UseSsl)
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
             {
-                var sslStream = new SslStream(_stream, false);
-                string targetHost = Endpoint switch
-                {
-                    IPEndPoint ip => ip.Address.ToString(),
-                    DnsEndPoint dns => dns.Host,
-                    _ => throw new ArgumentException($"Unknown EndPoint type: {(Endpoint.GetType())}")
-                };
+                if (_state == State.Open)
+                    throw new InvalidOperationException("Client was already connected.");
 
-                var sslOptions = new SslClientAuthenticationOptions
-                {
-                    TargetHost = targetHost,
-                };
+                await _socket.ConnectAsync(Endpoint, cancellationToken).ConfigureAwait(false);
 
-                await sslStream.AuthenticateAsClientAsync(sslOptions, cancellationToken).ConfigureAwait(false);
-                _stream = sslStream;
+                _stream = new NetworkStream(_socket);
+
+                if (UseSsl)
+                {
+                    var sslStream = new SslStream(_stream, false);
+                    string targetHost = Endpoint switch
+                    {
+                        IPEndPoint ip => ip.Address.ToString(),
+                        DnsEndPoint dns => dns.Host,
+                        _ => throw new ArgumentException($"Unknown EndPoint type: {(Endpoint.GetType())}")
+                    };
+
+                    var sslOptions = new SslClientAuthenticationOptions
+                    {
+                        TargetHost = targetHost,
+                    };
+
+                    await sslStream.AuthenticateAsClientAsync(sslOptions, cancellationToken).ConfigureAwait(false);
+                    _stream = sslStream;
+                }
+
+                _pipeReader = PipeReader.Create(_stream);
+                _readTask = ReadPipeAsync(_pipeReader);
             }
-
-            _readTask = ReadStreamAsync(_stream);
+            finally
+            {
+                _connectSem.Release();
+            }
         }
 
         public async Task CloseAsync(CancellationToken cancellationToken = default)
         {
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
             try
             {
-                _socket.Close();
+                if (_state != State.Open) return;
+
+                _state = State.CloseRequested;
+
+                try
+                {
+                    _socket.Close();
+                }
+                catch { }
+
+                _pipeReader!.CancelPendingRead();
+                await _readTask!.ConfigureAwait(false);
+
+                _readTask = null;
+                _pipeReader = null;
+
+                _stream!.Dispose();
+                _stream = null;
+
+                _state = State.Closed;
             }
-            catch { }
+            finally
+            {
+                _connectSem.Release();
+            }
+        }
 
-            _pipeReader?.CancelPendingRead();
+        private async Task UnexpectedCloseAsync(Exception? exception, CancellationToken cancellationToken = default)
+        {
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
+            {
+                if (_state != State.Open) return;
 
-            if (_readTask is Task readTask)
-                await readTask.ConfigureAwait(false);
+                _socket.Close();
 
-            _readTask = null;
-            _pipeReader = null;
+                // Cannot await, would deadlock
+                _readTask = null;
+                _pipeReader = null;
 
-            _stream?.Dispose();
+                _stream!.Dispose();
+                _stream = null;
+
+                _state = State.Closed;
+            }
+            finally
+            {
+                _connectSem.Release();
+            }
+
+            ClosedUnexpectedly?.Invoke(exception);
         }
 
         public async ValueTask SendAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         {
+            // TODO
+            if (_state != State.Open)
+                throw new InvalidOperationException($"Cannot send while not open. Current state: {_state}");
             // TODO: Flush?
             await _stream!.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
         }
 
-        private async Task ReadStreamAsync(Stream stream)
+        private async Task ReadPipeAsync(PipeReader reader)
         {
-            // TODO: Handle exceptions
-
-            var reader = PipeReader.Create(stream);
-            _pipeReader = reader;
-
+            Exception? closeException = null;
             try
             {
                 while (true)
@@ -111,14 +176,14 @@ namespace Twitch.Clients
 
                     try
                     {
-                        while (TryReadLine(ref buffer, out ReadOnlySequence<byte> line))
+                        while (ReadOnlySequenceExtensions.TryReadMessage(ref buffer, out ReadOnlySequence<byte> message, MessageDelimiter))
                         {
                             var evt = Received;
                             if (evt is not null)
                             {
                                 try
                                 {
-                                    await evt.Invoke(line).ConfigureAwait(false);
+                                    await evt.Invoke(message).ConfigureAwait(false);
                                 }
                                 catch (Exception ex)
                                 {
@@ -137,32 +202,25 @@ namespace Twitch.Clients
             }
             catch (Exception ex)
             {
+                closeException = ex;
                 _logger.LogError(ex, "Exception in reader task.");
             }
             finally
             {
                 await reader.CompleteAsync().ConfigureAwait(false);
+
+                _logger.LogDebug("Read task completed.");
             }
+
+            if (_state != State.CloseRequested)
+                await UnexpectedCloseAsync(closeException).ConfigureAwait(false);
         }
 
-        private static bool TryReadLine(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> line)
+        public void Dispose()
         {
-            // Every message ends with "\r\n"
-            SequencePosition? currentEndPos = buffer.PositionOf((byte)'\r');
-
-            if (!currentEndPos.HasValue)
-            {
-                line = default;
-                return false;
-            }
-
-            line = buffer.Slice(0, currentEndPos.Value);
-
-            // Skip the line + "\r\n"
-            SequencePosition nextStartPos = buffer.GetPosition(2, currentEndPos.Value);
-            buffer = buffer.Slice(nextStartPos);
-
-            return true;
+            _socket.Dispose();
+            _connectSem.Dispose();
+            _stream?.Dispose();
         }
     }
 }
--- src/Common/Clients/TcpSocketClient.cs
+++ src/Common/Clients/TcpSocketClient.cs
@@ -10,87 +10,152 @@ using Microsoft.Extensions.Logging;
 
 namespace Twitch.Clients
 {
-    // TODO: finish, public
-    internal class TcpSocketClient : ISocketClient
+    public class TcpSocketClient : ISocketClient
     {
         private readonly ILogger<TcpSocketClient> _logger;
         private readonly TcpClient _tcpClient;
+        private readonly SemaphoreSlim _connectSem;
         private Stream? _stream;
         private PipeReader? _pipeReader;
         private Task? _readTask;
+        private State _state;
+        private string _host;
 
         public TcpSocketClient(string host, int port, bool ssl, ILogger<TcpSocketClient> logger)
         {
-            Host = host;
+            _host = host ?? throw new ArgumentNullException(nameof(host));
             Port = port;
             UseSsl = ssl;
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _tcpClient = new TcpClient();
+            _connectSem = new SemaphoreSlim(1, 1);
+            _state = State.None;
         }
 
         public event Func<ReadOnlySequence<byte>, ValueTask>? Received;
-
-#pragma warning disable CS0067 // The event is never used
         public event Func<Exception?, ValueTask>? ClosedUnexpectedly;
-#pragma warning restore CS0067
 
-        public string Host { get; set; }
+        private enum State
+        {
+            None,
+            Open,
+            CloseRequested,
+            Closed,
+        }
+
+        public string Host
+        {
+            get => _host;
+            set => _host = value ?? throw new ArgumentNullException(nameof(Host));
+        }
+
         public int Port { get; set; }
         public bool UseSsl { get; set; }
+        public byte[] MessageDelimiter { get; set; } = new byte[] { (byte)'\r', (byte)'\n' };
 
         public async Task ConnectAsync(CancellationToken cancellationToken = default)
         {
-            await _tcpClient.ConnectAsync(Host, Port, cancellationToken).ConfigureAwait(false);
-
-            _stream = _tcpClient.GetStream();
-
-            if (UseSsl)
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
             {
-                var sslStream = new SslStream(_stream, false);
-                var sslOptions = new SslClientAuthenticationOptions
+                if (_state == State.Open)
+                    throw new InvalidOperationException("Client was already connected.");
+
+                await _tcpClient.ConnectAsync(Host, Port, cancellationToken).ConfigureAwait(false);
+
+                _stream = _tcpClient.GetStream();
+
+                if (UseSsl)
                 {
-                    TargetHost = Host,
-                };
+                    var sslStream = new SslStream(_stream, false);
+                    var sslOptions = new SslClientAuthenticationOptions
+                    {
+                        TargetHost = Host,
+                    };
 
-                await sslStream.AuthenticateAsClientAsync(sslOptions, cancellationToken).ConfigureAwait(false);
-                _stream = sslStream;
+                    await sslStream.AuthenticateAsClientAsync(sslOptions, cancellationToken).ConfigureAwait(false);
+                    _stream = sslStream;
+                }
+
+                _pipeReader = PipeReader.Create(_stream);
+                _readTask = ReadPipeAsync(_pipeReader);
             }
-
-            _readTask = ReadStreamAsync(_stream);
+            finally
+            {
+                _connectSem.Release();
+            }
         }
 
         public async Task CloseAsync(CancellationToken cancellationToken = default)
         {
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
             try
             {
-                _tcpClient.Close();
+                if (_state != State.Open) return;
+
+                _state = State.CloseRequested;
+
+                try
+                {
+                    _tcpClient.Close();
+                }
+                catch { }
+
+                _pipeReader!.CancelPendingRead();
+                await _readTask!.ConfigureAwait(false);
+
+                _readTask = null;
+                _pipeReader = null;
+
+                _stream!.Dispose();
+                _stream = null;
+
+                _state = State.Closed;
             }
-            catch { }
+            finally
+            {
+                _connectSem.Release();
+            }
+        }
 
-            _pipeReader?.CancelPendingRead();
+        private async Task UnexpectedCloseAsync(Exception? exception, CancellationToken cancellationToken = default)
+        {
+            await _connectSem.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
+            {
+                if (_state != State.Open) return;
 
-            if (_readTask is Task readTask)
-                await readTask.ConfigureAwait(false);
+                _tcpClient.Close();
 
-            _readTask = null;
-            _pipeReader = null;
+                // Cannot await, would deadlock
+                _readTask = null;
+                _pipeReader = null;
 
-            _stream?.Dispose();
+                _stream!.Dispose();
+                _stream = null;
+
+                _state = State.Closed;
+            }
+            finally
+            {
+                _connectSem.Release();
+            }
+
+            ClosedUnexpectedly?.Invoke(exception);
         }
 
         public async ValueTask SendAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         {
+            // TODO
+            if (_state != State.Open)
+                throw new InvalidOperationException($"Cannot send while not open. Current state: {_state}");
             // TODO: Flush?
             await _stream!.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
         }
 
-        private async Task ReadStreamAsync(Stream stream)
+        private async Task ReadPipeAsync(PipeReader reader)
         {
-            // TODO: Handle exceptions
-
-            var reader = PipeReader.Create(stream);
-            _pipeReader = reader;
-
+            Exception? closeException = null;
             try
             {
                 while (true)
@@ -103,14 +168,14 @@ namespace Twitch.Clients
 
                     try
                     {
-                        while (TryReadLine(ref buffer, out ReadOnlySequence<byte> line))
+                        while (ReadOnlySequenceExtensions.TryReadMessage(ref buffer, out ReadOnlySequence<byte> message, MessageDelimiter))
                         {
                             var evt = Received;
                             if (evt is not null)
                             {
                                 try
                                 {
-                                    await evt.Invoke(line).ConfigureAwait(false);
+                                    await evt.Invoke(message).ConfigureAwait(false);
                                 }
                                 catch (Exception ex)
                                 {
@@ -129,32 +194,25 @@ namespace Twitch.Clients
             }
             catch (Exception ex)
             {
+                closeException = ex;
                 _logger.LogError(ex, "Exception in reader task.");
             }
             finally
             {
                 await reader.CompleteAsync().ConfigureAwait(false);
+
+                _logger.LogDebug("Read task completed.");
             }
+
+            if (_state != State.CloseRequested)
+                await UnexpectedCloseAsync(closeException).ConfigureAwait(false);
         }
 
-        private static bool TryReadLine(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> line)
+        public void Dispose()
         {
-            // Every message ends with "\r\n"
-            SequencePosition? currentEndPos = buffer.PositionOf((byte)'\r');
-
-            if (!currentEndPos.HasValue)
-            {
-                line = default;
-                return false;
-            }
-
-            line = buffer.Slice(0, currentEndPos.Value);
-
-            // Skip the line + "\r\n"
-            SequencePosition nextStartPos = buffer.GetPosition(2, currentEndPos.Value);
-            buffer = buffer.Slice(nextStartPos);
-
-            return true;
+            _tcpClient.Dispose();
+            _connectSem.Dispose();
+            _stream?.Dispose();
         }
     }
 }
